fae84d0d8cde44e0e71c044a7550cace27c36dfe
Carlo Supina <cdsupina@gmail.com>
2020-01-06 13:22:16 -0600

    add uninstall subcommand and help when no args given

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use clap::{App, Arg};
+use clap::{App, AppSettings, Arg};
 mod install;
 
-static GLOBAL_FP_LIB_TABLE: &str = ".test/config/kicad/fp-lib-table";
-static GLOBAL_SYM_LIB_TABLE: &str = ".test/config/kicad/sym-lib-table";
+//static GLOBAL_FP_LIB_TABLE: &str = ".test/config/kicad/fp-lib-table";
+//static GLOBAL_SYM_LIB_TABLE: &str = ".test/config/kicad/sym-lib-table";
 
 fn main() {
     let matches = App::new("kibrarian")
         .version("0.1.0")
         .author("Carlo Supina <cdsupina@micronote.tech>")
         .about("A library manager for Kicad.")
+        .setting(AppSettings::ArgRequiredElseHelp)
         .subcommand(
             App::new("install")
                 .about("Installs a library.")
@@ -21,6 +22,22 @@ fn main() {
                 .arg(
                     Arg::with_name("target")
                         .help("Target library to install.")
+                        .index(1)
+                        .required(true),
+                ),
+        )
+        .subcommand(
+            App::new("uninstall")
+                .about("Uninstalls a library.")
+                .arg(
+                    Arg::with_name("global")
+                        .help("Indicate global.")
+                        .short("g")
+                        .long("global"),
+                )
+                .arg(
+                    Arg::with_name("target")
+                        .help("Target library to uninstall.")
                         .index(1)
                         .required(true),
                 ),
@@ -48,6 +65,18 @@ fn main() {
             ) {
                 Ok(()) => {}
                 Err(e) => println!("error: {}", e),
+            }
+        }
+
+        ("uninstall", Some(uninstall_matches)) => {
+            println!(
+                "Uninstalling {}",
+                uninstall_matches.value_of("target").unwrap()
+            );
+            if uninstall_matches.is_present("global") {
+                println!("uninstalling globally");
+            } else {
+                println!("uninstalling for project");
             }
         }
 
