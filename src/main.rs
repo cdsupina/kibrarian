1d918baee8ea1cc3137164ff6bbe3a18b302398a
Carlo Supina <cdsupina@gmail.com>
2020-01-06 02:28:39 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+use clap::{App, Arg};
+mod install;
+
+static GLOBAL_FP_LIB_TABLE: &str = ".test/config/kicad/fp-lib-table";
+static GLOBAL_SYM_LIB_TABLE: &str = ".test/config/kicad/sym-lib-table";
+
+fn main() {
+    let matches = App::new("kibrarian")
+        .version("0.1.0")
+        .author("Carlo Supina <cdsupina@micronote.tech>")
+        .about("A library manager for Kicad.")
+        .subcommand(
+            App::new("install")
+                .about("Installs a library.")
+                .arg(
+                    Arg::with_name("global")
+                        .help("Indicate global.")
+                        .short("g")
+                        .long("global"),
+                )
+                .arg(
+                    Arg::with_name("target")
+                        .help("Target library to install.")
+                        .index(1)
+                        .required(true),
+                ),
+        )
+        .get_matches();
+
+    // global install
+    // install libraries to KICAD_LIBRARY_DIR
+    // update GLOBAL_FP_LIB_TABLE and SYM_LIB_TABLE
+    match matches.subcommand() {
+        ("install", Some(install_matches)) => {
+            println!(
+                "Installing {} ",
+                install_matches.value_of("target").unwrap()
+            );
+            if install_matches.is_present("global") {
+                println!("installing globally");
+            } else {
+                println!("installing for project");
+            }
+
+            match install::install(
+                install_matches.is_present("global"),
+                install_matches.value_of("target").unwrap(),
+            ) {
+                Ok(()) => {}
+                Err(e) => println!("error: {}", e),
+            }
+        }
+
+        _ => unreachable!(),
+    }
+}
